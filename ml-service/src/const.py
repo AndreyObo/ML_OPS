e8789a23113f029afa1613e4ad73fba8c84317be
Andrey Obozov <AnAObozov@rosatom.ru>
2026-05-26 18:07:57 +0300

    fix default port

--- ml-service/src/const.py
+++ ml-service/src/const.py
@@ -2,7 +2,7 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-LOG_URL = os.environ.get('LOG_URL', 'http://log-service:6000/log')
+LOG_URL = os.environ.get('LOG_URL', 'http://log-service:5002/log')
 
 MODELS_DIR = os.path.join(current_dir, 'models')
 DATA_DIR =  os.path.join(current_dir, 'data')
